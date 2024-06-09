f4c663be98f39b274bb29c86bd8b724c7a823313
Harsh Thakkar <65072835+Harsh971@users.noreply.github.com>
2024-06-09 12:20:57 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,8 +5,8 @@ echo "First 5 biggest file in the file system passed via positional argument"
 path="$1"
 echo $path
 
-du -ah $path | sort -hr | head -n 5 > /home/harsh/Desktop/filesize.txt
+du -ah $path | sort -hr | head -n 5 
 
 echo "This is the list of big files in the file system $path "
 
-cat /home/harsh/Desktop/filesize.txt
+
